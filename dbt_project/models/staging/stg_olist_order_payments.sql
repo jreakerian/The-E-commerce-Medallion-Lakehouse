752ce043a8506b3d486c282e192d48465daa7e51
kerian <kerianeminence7@gmail.com>
2026-05-29 11:45:43 -0400

    feat: implement silver layer staging models with explicit schema enforcement

---
+++ dbt_project/models/staging/stg_olist_order_payments.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('raw_bronze', 'olist_order_payments_dataset') }}
+
+),
+
+renamed as (
+
+    select
+        order_id::varchar as order_id,
+        payment_sequential::number as payment_sequential,
+        payment_type::varchar as payment_type,
+        payment_installments::number as payment_installments,
+        payment_value::number(10,2) as payment_value
+
+    from source
+
+)
+
+select * from renamed
