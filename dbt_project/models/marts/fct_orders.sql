d0bc665cd06130734a7d4f17e8f0b9655f9dd976
kerian <kerianeminence7@gmail.com>
2026-05-29 12:41:52 -0400

    feat: develop core fct_orders Description: Engineered fct_orders in the Gold layer, centralizing order metrics including total price, freight, and payment values. Integrated aggregated metrics from stg_olist_order_items and pivoted payment data from the intermediate layer.

---
+++ dbt_project/models/marts/fct_orders.sql
@@ -0,0 +1,38 @@
+WITH orders AS (
+    SELECT * FROM {{ ref('stg_olist_orders') }}
+),
+
+orders_items AS (
+    SELECT
+        order_id,
+        SUM(price) AS total_price,
+        SUM(freight_value) AS total_freight_value,
+        COUNT(order_item_id) AS number_of_items
+    FROM {{ ref('stg_olist_order_items') }}
+    GROUP BY order_id
+),
+
+order_payments AS (
+    SELECT
+        order_id,
+        total_amount AS total_payment_value
+    FROM {{ ref('int_order_payments_pivoted')}}
+),
+
+final AS (
+    SELECT
+        orders.order_id,
+        orders.customer_id,
+        orders.order_status,
+        orders.order_purchase_timestamp,
+        orders.order_delivered_customer_date,
+        COALESCE(orders_items.total_price, 0) AS total_price,
+        COALESCE(orders_items.total_freight_value, 0) AS total_freight_value,
+        COALESCE(orders_items.number_of_items, 0) AS number_of_items,
+        COALESCE(order_payments.total_payment_value, 0) AS total_payment_value
+    FROM orders
+    LEFT JOIN orders_items ON orders.order_id = orders_items.order_id
+    LEFT JOIN order_payments ON orders.order_id = order_payments.order_id
+)
+
+SELECT * FROM final
